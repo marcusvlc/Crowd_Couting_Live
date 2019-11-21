01fcd4f0da073f99f91ebac14eb6228eb85362c2
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2019-11-21 11:38:13 -0300

    Live couting demo

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Crowd_Couting_Live
+
+Repository created for demonstration of a Crowd Couting algorithm in real time.
+
+![Live Crowd Couting Demo](./images/crowd.png)
+
+This repository uses the CANNet network to make inferences in real time. You can find out how to train, test, and other information about it in [this repository.](https://github.com/CommissarMa/Context-Aware_Crowd_Counting-pytorch)
+
+
+# How to install
+
+To install the required dependencies, you will need:
+
+- Python 3.6 or higher
+- pip
+
+```
+pip install -r requirements.txt
+```
+
+
+# How to run
+
+Before running the application, it is important to understand what parameters the live_couting.py file receives
+
+
+**--stream** *"url or path"*
+
+Receives the string that represents your stream. This string can be a path to some video on your machine, your webcam id or even an RTSP url for a security camera.
+
+**--weights** *"directory"*
+
+It receives the directory where the pre-trained CANNet model file is located. By default, we make available in this repository a pre-trained model within the checkpoints folder provided by the repository mentioned above.
+
+```
+$ python live_couting.py --stream "url" --weights "path_to_weights"
+```
+
+# How to contribute
+
+We appreciate all kind of contribution. If you find it necessary to increase some functionality, just fork the repository and create a pull request with the following information:
+
+- What you added / removed / edited
+- Why was this change necessary
+
+If you have any questions, feel free to open an Issue.
+
---
+++ live_couting.py
@@ -0,0 +1,59 @@
+import torch
+import matplotlib.pyplot as plt
+from cannet import CANNet
+from torchvision import transforms
+import argparse
+import cv2
+import copy
+import os
+
+def main_live(args):
+    
+    capture = cv2.VideoCapture(args.stream)
+    num_gpus = torch.cuda.device_count()
+    device='cpu'
+    #device = 'cuda' if num_gpus >= 1 else 'cpu'
+    model=CANNet().to(device)
+    model.load_state_dict(torch.load(args.weights,map_location=torch.device(device)))
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    model.eval()
+    while(capture.isOpened()):
+        ret, frame = capture.read()
+        if(ret):
+            key = cv2.waitKey(1) & 0xFF
+
+            copy_frame = copy.copy(frame)
+
+            copy_frame = transform(copy_frame)
+            
+            # putting the batch dimension on tensor
+            copy_frame = copy_frame.unsqueeze(0)
+            copy_frame = copy_frame.to(device)
+            result = model(copy_frame)
+
+            crowd_count = int(result.data.sum().item())
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, "Crowd Couting: " + str(crowd_count) , (10,35), font, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.imshow("Frame", frame)
+
+            print(crowd_count)
+
+            if key == ord("q"):
+                break
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Crowd Couting Network')
+    parser.add_argument('--stream', default=0, type=str,
+                        help='The url stream for live couting')
+    parser.add_argument('--weights', default="./checkpoints/cvpr2019_CAN_SHHA_353.pth", type=str,
+                        help='The path to the weights .pt or .pth file')
+
+
+    args = parser.parse_args()
+
+    main_live(args)
+
